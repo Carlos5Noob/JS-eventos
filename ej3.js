3dff36d509b72ecbb67d3265d7a72ea1683a29a6
Carlos5Noob <cchaati2101@fp.iesromerovargas.com>
2025-01-24 09:23:34 +0100

    ej3 refactored

--- ej3.js
+++ ej3.js
@@ -1,44 +1,12 @@
-function informacion(elEvento) {
-    let evento = elEvento || window.event;
-    let x = evento.clientX;
-    let y = evento.clientY;
-    let dimensiones = tamanoVentanaNavegador();
+let parrafo = document.querySelector("#parrafo");
 
-    let zona = "";
-    if (x < dimensiones[0] / 2 && y < dimensiones[1] / 2) {
-        zona = "Esquina superior izquierda";
-    } else if (x >= dimensiones[0] / 2 && y < dimensiones[1] / 2) {
-        zona = "Esquina superior derecha";
-    } else if (x < dimensiones[0] / 2 && y >= dimensiones[1] / 2) {
-        zona = "Esquina inferior izquierda";
-    } else {
-        zona = "Esquina inferior derecha";
-    }
+document.addEventListener("keydown", muestraCaracter)
 
-    muestraInformacion([
-        "Zona clicada",
-        `Coordenadas: (${x}, ${y})`,
-        `Zona: ${zona}`
-    ]);
+function muestraCaracter(e) {
+    parrafo.innerHTML = `
+    Tecla: [${e.key}]
+    Código: [${e.keyCode}]
+    `
 }
 
-function muestraInformacion(mensaje) {
-    document.getElementById("info").innerHTML = '<h1>' + mensaje[0] + '</h1>';
-    for (let i = 1; i < mensaje.length; i++) {
-        document.getElementById("info").innerHTML += '<p>' + mensaje[i] + '</p>';
-    }
-}
-
-function tamanoVentanaNavegador() {
-    let dimensiones = [];
-    if (typeof(window.innerWidth) == 'number') {
-        dimensiones = [window.innerWidth, window.innerHeight];
-    } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
-        dimensiones = [document.documentElement.clientWidth, document.documentElement.clientHeight];
-    } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
-        dimensiones = [document.body.clientWidth, document.body.clientHeight];
-    }
-    return dimensiones;
-}
-
-document.onclick = informacion;
+document.getElementById("info").style.backgroundColor = "skyblue";
